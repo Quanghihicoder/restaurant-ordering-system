d6d320f6e2b6f28af50acfea93752ab4a8201031
Quanghihicoder <hoangquang2508@gmail.com>
2024-06-17 16:27:46 +1000

    change

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 window.axios = axios
 axios.defaults.withCredentials = false
-axios.defaults.baseURL = "http://localhost:8000/api"
-// let backendUrl = "http://" + window.location.hostname.toString() + ":8000/api"
-// axios.defaults.baseURL = backendUrl
+// axios.defaults.baseURL = "http://localhost:8000/api"
+let backendUrl = "http://" + window.location.hostname.toString() + ":8000/api"
+axios.defaults.baseURL = backendUrl
