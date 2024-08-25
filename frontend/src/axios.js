059fca152e61e02dc57225c8fe79a15fa08ca57b
Quanghihicoder <hoangquang2508@gmail.com>
2024-08-25 23:16:28 +1000

    new build

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 window.axios = axios
 axios.defaults.withCredentials = false
 // axios.defaults.baseURL = "http://localhost:8000/api"
-let backendUrl = "http://" + window.location.hostname.toString() + ":8000/api"
+let backendUrl = "http://" + window.location.hostname.toString() + ":8001/api"
 axios.defaults.baseURL = backendUrl
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/': {
-        target: 'http://localhost:8000'
+        target: 'http://localhost:8001'
       }
     }
   }
