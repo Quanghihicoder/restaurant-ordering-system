70e6afe4a6bc58f7963e3d84229bec795ce48bab
Quanghihicoder <hoangquang2508@gmail.com>
2024-08-25 23:05:27 +1000

    change port

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,7 @@ app.get('/*', function (req, res) {
 
 
 // PORT
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
