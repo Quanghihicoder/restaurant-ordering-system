e8513fdf845a80e081b5e90ab41daf8557d64d4b
Quanghihicoder <hoangquang2508@gmail.com>
2025-11-11 19:17:02 +1100

    fix

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -3,41 +3,40 @@ import db from "../config/database.js";
 
 // get all user
 export const getAllUser = (result) => {
-    db.query("SELECT * FROM user", (err,results)=> {
-        if (err){
+    db.query("SELECT * FROM user", (err, results) => {
+        if (err) {
             console.log(err);
-            result(err,null);
-        }else{
-            result(null,results[0]);
+            result(err, null);
+        } else {
+            result(null, results[0]);
         }
     });
 };
 
 
 // get single user
-export const getUserByEmail = (data,result) => {
-    db.query("SELECT user_id, user_name, user_password FROM user WHERE user_email = ?",[data], (err,results)=> {
-        if (err){
+export const getUserByEmail = (data, result) => {
+    db.query("SELECT user_id, user_name, user_password, user_email, user_phone FROM user WHERE user_email = ?", [data], (err, results) => {
+        if (err) {
             console.log(err);
-            result(err,null);
-        }else{
-            result(null,results[0]);
+            result(err, null);
+        } else {
+            result(null, results[0]);
         }
     });
 };
 
 // insert User
-export const insertUser = (data,result) => {
-    db.query("INSERT INTO user SET ?",data, (err,results)=> {
-        if (err){
+export const insertUser = (data, result) => {
+    db.query("INSERT INTO user SET ?", data, (err, results) => {
+        if (err) {
             console.log(err);
-            result(err,null);
-        }else{
-            result(null,results[0]);
+            result(err, null);
+        } else {
+            result(null, results[0]);
         }
     });
 };
-
 
 
 
